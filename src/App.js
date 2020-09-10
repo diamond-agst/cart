8e9785793da6f62ed955db6e716c53bf3801beaa
diamond-agst <diamondagst@gmail.com>
2020-09-10 18:02:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home';
+import Footer from './components/pages/Footer/Footer';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={Home} />
+      <Route path='/services' component={Services} />
+      <Route path='/products' component={Products} />
+      <Route path='/sign-up' component={SignUp} />
+      <Footer />
+    </Router>
+  )
+}
+
+export default App;
